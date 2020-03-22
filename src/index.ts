6e24f03bd1b529833e7cf458b1f645e8791ae862
Jonathan Brenman <jonathanbrenman@gmail.com>
2020-03-21 21:20:21 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,33 @@
 const { Elevator } = require('./classes/elevator');
 const { Person }   = require('./classes/person');
 
+
+var init = (personFloor, person, elevator) => {
+    person.openDoor();
+    person.closeDoor();
+    person.walkToElevator();
+    let isElevatorArrived = elevator.requestElevator(personFloor);
+    if (isElevatorArrived) {
+        person.takeElevator();
+        elevator.downToPB();
+        person.goOut();
+    } else {
+        // no se puede usar el ascensor voy por escalera
+        person.downStairs();
+        person.goOut();
+    }
+};
+
+console.log("----------------------- Inicio caso 1 -----------------------");
 var personFloor = 5; // La persona se encuentra en el piso 5
 var person      = new Person("pepe");
 var elevator    = new Elevator(10, 0, "ok");
+init(personFloor, person, elevator);
+console.log("----------------------- Finalizo caso 1 -----------------------");
 
-console.log("Program Started.");
-person.openDoor();
-person.closeDoor();
-person.walkToElevator();
-let isElevatorArrived = elevator.requestElevator(personFloor);
-if (isElevatorArrived) {
-    person.takeElevator();
-    elevator.downToPB();
-    person.goOut();
-} else {
-    // no se puede usar el ascensor voy por escalera
-    person.downStairs();
-    person.goOut();
-}
-console.log("Program Finished.");
+console.log("----------------------- Inicio caso 2 -----------------------");
+var personFloor = 3; // La persona se encuentra en el piso 3
+var person      = new Person("juaquin");
+var elevator    = new Elevator(10, 0, "fuera de servicio");
+init(personFloor, person, elevator);
+console.log("----------------------- Finalizo caso 2 -----------------------");
